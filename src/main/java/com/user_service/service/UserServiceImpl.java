9ffaaa01326ca0fb13c8c2429268875e0e85c960
Kuleb9lka <winkeylinks@gmail.com>
2025-12-04 13:46:50 +0300

    added aop logs and direct logs

--- src/main/java/com/user_service/service/UserServiceImpl.java
+++ src/main/java/com/user_service/service/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.user_service.exception.UsernameIsAlreadyExistException;
 import com.user_service.mapper.UserMapper;
 import com.user_service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
@@ -31,6 +33,8 @@ public class UserServiceImpl implements UserService {
     public UserResponseDto getById(Long id) {
 
         User userById = getUserById(id);
+
+        log.info("Found user by ID {}", id);
 
         return mapper.toResponseDto(userById);
     }
@@ -42,6 +46,8 @@ public class UserServiceImpl implements UserService {
 
         List<User> allUsers = repository.findAll(userSpecification);
 
+        log.info("Found {} users available for this search request", allUsers.size());
+
         return mapper.toResponseDtoList(allUsers);
     }
 
@@ -49,14 +55,19 @@ public class UserServiceImpl implements UserService {
     public UserResponseDto createWithRole(UserRequestDto dto, UserRole role) {
 
         checkUniqueUsername(dto.getUsername());
-
         checkUniqueEmail(dto.getEmail());
+
+        log.info("Username: {} and Email: {} is unique", dto.getUsername(), dto.getEmail());
 
         User user = mapper.toEntityFromRequestDto(dto);
         user.setRole(role);
         user.setIsActive(true);
 
+        log.info("Start saving user");
+
         User savedUser = repository.save(user);
+
+        log.info("User was successfully saved");
 
         return mapper.toResponseDto(savedUser);
     }
@@ -66,9 +77,15 @@ public class UserServiceImpl implements UserService {
 
         User userById = getUserById(dto.getId());
 
+        log.info("Found user by id {}", userById.getId());
+
         mapper.updateUserFromDb(dto, userById);
 
+        log.info("Start saving user");
+
         repository.save(userById);
+
+        log.info("User was successfully saved");
     }
 
     @Override
@@ -79,20 +96,30 @@ public class UserServiceImpl implements UserService {
 
     private User getUserById(Long id){
 
+        log.info("Get UserById method, id: {}", id);
+
         return repository.findById(id).orElseThrow(() -> new UserNotFoundException(ExceptionConstant.USER_NOT_FOUND_BY_ID + id));
     }
 
     private void checkUniqueEmail(String email){
 
+        log.info("Checking unique email: {}", email);
+
         repository.findByEmail(email).ifPresent(e -> {
             throw new EmailIsAlreadyExistException(ExceptionConstant.EMAIL_IS_ALREADY_EXIST + email);
         });
+
+        log.info("Email {} is unique", email);
     }
 
     private void checkUniqueUsername(String username){
 
+        log.info("Checking unique email: {}", username);
+
         repository.findByUsername(username).ifPresent(e -> {
             throw new UsernameIsAlreadyExistException(ExceptionConstant.USERNAME_IS_ALREADY_EXIST + username);
         });
+
+        log.info("Username {} is unique", username);
     }
 }
