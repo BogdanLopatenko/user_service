9821e3e934e50ce0402ac17c19cf2dd891f7df63
Kuleb9lka <winkeylinks@gmail.com>
2026-01-20 16:26:34 +0300

    added UserEmailConfirmationService logging

--- src/main/java/com/user_service/mapper/UserEmailConfirmationMapper.java
+++ src/main/java/com/user_service/mapper/UserEmailConfirmationMapper.java
@@ -1,14 +1,11 @@
 package com.user_service.mapper;
 
-import com.user_service.dto.confirmation.UserEmailConfirmationRequestDto;
-import com.user_service.dto.confirmation.UserEmailConfirmationUpdateDto;
 import com.user_service.dto.confirmation.UserEmailConformationResponseDto;
 import com.user_service.entity.User;
 import com.user_service.entity.UserEmailConfirmation;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 import java.time.LocalDateTime;
@@ -24,10 +21,6 @@ public interface UserEmailConfirmationMapper {
 
         UserEmailConformationResponseDto toResponseDto(UserEmailConfirmation entity);
 
-        UserEmailConfirmation toEntityFromRequestDto(UserEmailConfirmationRequestDto dto);
-
         @Mapping(target = "token", ignore = true)
         UserEmailConfirmation construct(User user, LocalDateTime expiresAt, Boolean isUsed);
-
-        void updateFromDb(UserEmailConfirmationUpdateDto dto, @MappingTarget UserEmailConfirmation entity);
 }
--- src/main/java/com/user_service/service/impl/UserEmailConfirmationServiceImpl.java
+++ src/main/java/com/user_service/service/impl/UserEmailConfirmationServiceImpl.java
@@ -13,6 +13,7 @@ import com.user_service.repository.UserEmailConfirmationRepository;
 import com.user_service.service.UserEmailConfirmationService;
 import com.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserEmailConfirmationServiceImpl implements UserEmailConfirmationService {
 
     private final UserEmailConfirmationRepository userEmailConfirmationRepository;
@@ -39,18 +41,31 @@ public class UserEmailConfirmationServiceImpl implements UserEmailConfirmationSe
 
         UserEmailConfirmation userEmailConfirmationByToken = findByToken(token);
 
+        log.info("Email confirmation entity was successfully found");
+
         return userEmailConfirmationMapper.toResponseDto(userEmailConfirmationByToken);
     }
 
     @Override
     public UserEmailConformationResponseDto create(Long userId) {
 
+
+        log.info("Trying to get User by id: {}", userId);
+
         UserResponseDto userById = userService.getById(userId);
+
+        log.info("User was successfully found");
+
         User user = userMapper.toEntityFromResponseDto(userById);
 
+        log.debug("Constructing email confirmation entity");
         UserEmailConfirmation constructedEmailConfiguration = userEmailConfirmationMapper.construct(user, LocalDateTime.now().plusHours(expirationDurabilityInHours), false);
 
+        log.info("Trying to save Email confirmation entity");
+
         UserEmailConfirmation savedConfirmation = userEmailConfirmationRepository.save(constructedEmailConfiguration);
+
+        log.info("Email confirmation was successfully saved");
 
         return userEmailConfirmationMapper.toResponseDto(savedConfirmation);
     }
@@ -60,12 +75,21 @@ public class UserEmailConfirmationServiceImpl implements UserEmailConfirmationSe
 
         UserEmailConfirmation confirmationByToken = findByToken(dto.getToken());
 
+        log.info("Email confirmation entity was found");
+
         confirmationByToken.setIsUsed(true);
 
+        log.info("Trying to save email confirmation token");
+
         userEmailConfirmationRepository.save(confirmationByToken);
+
+        log.info("Email confirmation token was successfully found");
     }
 
     private UserEmailConfirmation findByToken(UUID token){
+
+        log.info("Tying to get entity by UUID token: {}", token);
+
         return userEmailConfirmationRepository.findById(token).orElseThrow(() -> new UserEmailConfirmationNotFoundException(ExceptionConstant.EMAIL_CONFIRMATION_NOT_FOUND_BY_TOKEN + token));
     }
 }
