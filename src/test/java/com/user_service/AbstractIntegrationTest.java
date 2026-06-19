69be726c63f1810f23279108097efa564a73c541
Kuleb9lka <winkeylinks@gmail.com>
2026-06-19 12:49:36 +0300

    created postgresql test container

---
+++ src/test/java/com/user_service/AbstractIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.user_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:17");
+}
